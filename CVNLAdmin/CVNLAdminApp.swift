9ace7cf26b05ece735bab876857c8ced72f634b0
Sang Ngo <heroandtn3@gmail.com>
2026-07-11 11:39:49 +0700

    Initial Commit

---
+++ CVNLAdmin/CVNLAdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  CVNLAdminApp.swift
+//  CVNLAdmin
+//
+//  Created by Sang Ngo on 11/7/26.
+//
+
+import SwiftUI
+
+@main
+struct CVNLAdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
